a2297bcad7e45a00055c05d1e9fc4bd6ad7b3663
msaityurguc-sudo <msaityurguc@gmail.com>
2026-07-30 22:00:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-// PERRE Yöresel Ürünler
-
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("PERRE sitesi hazır.");
 
-    // Gelecekte ürünler products.json dosyasından yüklenecek.
+    const productList = document.getElementById("product-list");
+
+    fetch("products.json")
+        .then(response => response.json())
+        .then(products => {
+
+            products.forEach(product => {
+
+                const card = document.createElement("div");
+
+                card.className = "card";
+
+                card.innerHTML = `
+                    <img src="${product.resim}" alt="${product.ad}" class="product-image">
+
+                    <h3>${product.ad}</h3>
+
+                    <p>${product.aciklama}</p>
+
+                    <h4>${product.fiyat} ₺ / ${product.birim}</h4>
+
+                    <a class="hero-btn"
+                    target="_blank"
+                    href="https://wa.me/905512530087?text=Merhaba,%20${encodeURIComponent(product.ad)}%20ürünü%20hakkında%20bilgi%20almak%20istiyorum.">
+                    WhatsApp Sipariş
+                    </a>
+                `;
+
+                productList.appendChild(card);
+
+            });
+
+        });
+
 });
