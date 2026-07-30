8993c0c6324098d999f531983ee4cd8704b345b5
msaityurguc-sudo <msaityurguc@gmail.com>
2026-07-30 21:54:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// PERRE Yöresel Ürünler
 
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("PERRE sitesi hazır.");
+
+    // Gelecekte ürünler products.json dosyasından yüklenecek.
+});
